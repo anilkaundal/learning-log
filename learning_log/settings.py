c854dc9029449c8a3eee6cc0c24a937c29d8e37c
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-06-30 17:15:00 +0530

    Some case changes & updated settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -123,8 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # My settings
 LOGIN_URL = '/users/login/'
@@ -146,7 +151,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow only Heroku to host the project.
-    ALLOWED_HOSTS = ['learning-log.herokuapp.com']
+    ALLOWED_HOSTS = ['learning-log-pad.herokuapp.com']
     DEBUG = False
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
